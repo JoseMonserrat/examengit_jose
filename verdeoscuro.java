c6debdc0aeff3a67ef75da3e347ee9c747d9a063
Camilo Albert <pepico.valencia@gmail.com>
2019-10-24 20:24:23 +0200

    añadido comentario y ordenar

--- verdeoscuro.java
+++ verdeoscuro.java
@@ -3,29 +3,29 @@ import java.util.Scanner;
 public class ejer472 {
 
         public static void main(String[] args) {
-
+          // introduce la variables
                 Scanner sc = new Scanner (System.in);
 
-                int desnivel = 0;
+                int desnivel=0;
                 int numveces;
-
+  
                 System.out.println("Introduzca el numero de ejemplos a introducir");
 
                 numveces = sc.nextInt();
                 System.out.println("Introduzca los desniveles");
+                
+                //hace un for para crear un bucle con if y else
                 for ( int i=0;i<numveces; i++) {
-                desnivel = sc.nextInt();
+                 desnivel = sc.nextInt();
 
                 }
                 if (desnivel > -1 && desnivel < 1000000 && numveces > -1 && numveces < 200000) {
                         System.out.println("APTA");
 
-
-
                 }else {
                         System.out.println(" NO APTA");
         }
-        }
+    }
 
 }
 
